3b677abbc96f044f7eba2bc29c1496979a79d071
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2023-01-29 15:00:06 -0300

    teste

--- main.js
+++ main.js
@@ -1,39 +1,36 @@
 const enviarItem = document.getElementById("novoitem");
-const itens =JSON.parse(localStorage.getItem("itens")) || [];
-console.log(itens);
-itens.forEach( (elemento)=> {
-    console.log(elemento);
-})
-
-enviarItem.addEventListener("submit",(evento)=>{
-    evento.preventDefault()
-nomeItem = evento.target.elements["nome"];
-quantidadeItem= evento.target.elements["quantidade"];
-
-const itemAtual = {
-    "nome": nomeItem.value,
-    "quantidade" : quantidadeItem.value,
-}
-itens.push(itemAtual);
-localStorage.setItem("itens", JSON.stringify(itens));
-
-
-    adicionaItens(nomeItem.value,quantidadeItem.value)
-nomeItem.value = "";
-quantidadeItem.value = "";
-})
-
-function adicionaItens(item){
-const linha = document.createElement('li');
-linha.classList.add("item");
-
-const numero= document.createElement('strong');
-numero.innerHTML = item.quantidade;
-linha.appendChild(numero);
-linha.innerHTML+= item.nome;
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
 const lista = document.getElementById("lista");
-lista.appendChild(linha);
+itens.forEach((elemento) => {
+    console.log(elemento.nome, elemento.quantidade);
+})
 
+enviarItem.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+    nomeItem = evento.target.elements["nome"];
+    quantidadeItem = evento.target.elements["quantidade"];
+    const itemAtual = {
+        "nome": nomeItem.value,
+        "quantidade": quantidadeItem.value,
+    }
+    itens.push(itemAtual);
+    localStorage.setItem("itens", JSON.stringify(itens));
+    adicionaItens(itemAtual);
+
+    nomeItem.value = "";
+    quantidadeItem.value = "";
+    
+})
+
+
+function adicionaItens(item) {
+    const linha = document.createElement('li');
+    linha.classList.add("item");
+
+    const numero = document.createElement('strong');
+    numero.innerHTML = item.quantidade;
+    linha.appendChild(numero);
+    linha.innerHTML += item.nome;
+    lista.appendChild(linha);
 
 }
-
